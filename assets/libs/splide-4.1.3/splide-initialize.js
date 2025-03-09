3a2cbe66d1704adbd56d308da679f37d07140885
Stefan Warnat <ich@stefanwarnat.de>
2025-03-09 22:05:41 +0000

    feat: add image and slideshow parameters

--- assets/libs/splide-4.1.3/splide-initialize.js
+++ assets/libs/splide-4.1.3/splide-initialize.js
@@ -1,8 +1,11 @@
 var splideData;
 if (splideData) {
   document.addEventListener("DOMContentLoaded", function (e) {
-    for (let id of splideData) {
-      new Splide("#" + id).mount();
+    for (let id in splideData) {
+      let options = Object.assign({
+        perPage: 1
+      }, splideData[id])
+      new Splide("#" + id, options).mount();
     }
   });
 }
